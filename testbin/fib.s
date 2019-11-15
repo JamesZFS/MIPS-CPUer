88ce1f2a312fe11c8710d53f68aa469e0ab3e273
zhengfs16 <zhengfs16@mails.tsinghua.edu.cn>
2019-11-15 22:00:44 +0800

    [add] start developing CPU 💻

---
+++ testbin/fib.s
@@ -0,0 +1,12 @@
+.set noreorder
+.set noat
+.globl __start
+.text
+__start:
+  ori $t0, $zero, 0x1 # t0 = 1
+  ori $t1, $zero, 0x1 # t1 = 1
+loop: addu $t2, $t0, $t1 # t2 = t0+t1
+  ori $t0, $t1, 0x0 # t0 = t1
+  ori $t1, $t2, 0x0 # t1 = t2
+  b loop
+  ori $zero, $zero, 0 # noop
