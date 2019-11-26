7674f2f078725ddff1529c07cbe167d8a602e5a4
zhengfs16 <zhengfs16@mails.tsinghua.edu.cn>
2019-11-26 16:14:49 +0800

    [update] uart-test, pretty sure uart works on my test!

--- testcases/uart_test.s
+++ testcases/uart_test.s
@@ -3,6 +3,21 @@
 .globl __start
 __start:
   li $a0,0xbfd003f0
+
+welcome:
+  li $a3, 0  # char counter
+  li $v0, len
+putc:
+  lb $a1,0xc($a0) # stat
+  andi $a1,$a1,1  # data sent?
+  beq $a1,$0,putc # wait until ready
+  nop
+  lb $a2,msg($a3)
+  sb $a2,0x8($a0) # send to uart
+  addiu $a3, 1
+  bne		$a3, $v0, putc
+  nop
+
 chk_rx:
   lb $a1,0xc($a0) # uart status
   andi $a1,$a1,2  # data ready?
@@ -22,5 +37,8 @@ chk_tx:
 #   bne $a1,$0,delay
   nop
   sb $a2,0x8($a0) # send to uart
-  j __start # forever
+  j chk_rx # forever
   nop
+
+msg: .ascii "UART initialized. "
+len = . - msg
